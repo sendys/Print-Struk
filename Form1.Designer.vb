f35329aa7568cda4b470f3dbc2f90e0ec9b9b45d
Sandi Saryono <sandy.saryono@gmail.com>
2020-06-01 21:18:06 +0700

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Qty = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Harga = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 221)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Print Struk"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.BackgroundColor = System.Drawing.Color.Gainsboro
+        Me.DataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        DataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle2.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle2
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Qty, Me.Harga, Me.Column2})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 41)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.Size = New System.Drawing.Size(550, 174)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Item Barang"
+        Me.Column1.Name = "Column1"
+        Me.Column1.Width = 300
+        '
+        'Qty
+        '
+        Me.Qty.HeaderText = "Qty"
+        Me.Qty.Name = "Qty"
+        Me.Qty.Width = 50
+        '
+        'Harga
+        '
+        Me.Harga.HeaderText = "Harga"
+        Me.Harga.Name = "Harga"
+        Me.Harga.Width = 80
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(335, 225)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 14)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Total Penjualan"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTotal.Location = New System.Drawing.Point(443, 222)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(103, 22)
+        Me.txtTotal.TabIndex = 3
+        Me.txtTotal.Text = "0"
+        Me.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Sub Total"
+        Me.Column2.Name = "Column2"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Trebuchet MS", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Red
+        Me.Label2.Location = New System.Drawing.Point(12, 10)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(333, 22)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "FORM PENJUALAN BARANG + STRUK KASIR"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(574, 256)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Transaksi Penjualan"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Qty As DataGridViewTextBoxColumn
+    Friend WithEvents Harga As DataGridViewTextBoxColumn
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,105 @@
+﻿
+Imports System.Drawing.Printing
+
+Public Class Form1
+
+    Dim NamaToko As String = "212 Mart Aceh"
+    Dim Alamat As String = "Jl. Mata Ie, Keutapang Dua"
+    Dim Kota As String = "Banda Aceh"
+
+    Dim Trans As String = "PJ20060001"
+    Dim Tgl As String = Format(Now, "yyyy-MM-dd HH:mm:ss")
+
+    Dim arrWidth() As Integer
+    Dim arrFormat() As StringFormat
+
+    Dim Header1() As Integer
+    Dim Header2() As StringFormat
+
+    Dim Footer1() As Integer
+    Dim Footer2() As StringFormat
+
+    Dim c As New PrintingFormat
+
+    Dim SubTotal As Double
+
+
+    Sub TotalPenjualan()
+        Dim total As Double
+
+        For Each i As DataGridViewRow In Me.DataGridView1.Rows
+            total += i.Cells(3).Value
+            txtTotal.Text = FormatNumber(total, 0)
+        Next
+    End Sub
+
+    Private Sub DataGridView1_CellEndEdit(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellEndEdit
+        If e.ColumnIndex = 2 Then
+            Me.DataGridView1.Rows(e.RowIndex).Cells(3).Value = Me.DataGridView1.Rows(e.RowIndex).Cells(1).Value * Me.DataGridView1.Rows(e.RowIndex).Cells(2).Value
+            TotalPenjualan()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Printer.NewPrint()
+        Printer.Print(" ")
+
+        Header1 = {250, 0} : Header2 = {c.MidCenter}
+
+        Printer.SetFont("Courier New", 11, FontStyle.Bold)
+        Printer.Print(NamaToko, Header1, Header2)
+
+        Printer.SetFont("Courier New", 8, FontStyle.Regular)
+        Printer.Print(Alamat, {250}, 0)
+
+        Printer.SetFont("Courier New", 8, FontStyle.Regular)
+        Printer.Print(Kota, {250}, 0)
+
+        Printer.Print("-----------------------------------")
+        Printer.Print("Transaksi : " & Trans)
+        Printer.Print("Tgl       : " & Tgl)
+        Printer.Print("Kasir     : Sandi")
+        Printer.Print("-----------------------------------")
+
+        Printer.SetFont("Courier New", 8, FontStyle.Bold)
+        arrWidth = {90, 80, 75}
+        arrFormat = {c.MidLeft, c.MidRight, c.MidRight}
+
+        'nama kolom dipisah dengan ;
+        Printer.Print("Item Barang#Qty#Sub Total", arrWidth, arrFormat)
+        Printer.SetFont("Courier New", 8, FontStyle.Regular)
+        arrWidth = {135, 32, 70}
+        arrFormat = {c.MidLeft, c.MidRight, c.MidRight}
+
+        Printer.Print("-----------------------------------")
+
+        'loop item penjualan
+        For Each xrow As DataGridViewRow In Me.DataGridView1.Rows
+            Printer.Print(Strings.Left(xrow.Cells(0).Value, 15) & "#" & xrow.Cells(1).Value & "#" &
+                         (FormatNumber(xrow.Cells(1).Value * xrow.Cells(2).Value, 0)), arrWidth, arrFormat)
+            SubTotal += (xrow.Cells(1).Value * xrow.Cells(2).Value)
+        Next
+
+        Printer.Print("-----------------------------------")
+        arrWidth = {130, 110}
+        arrFormat = {c.MidLeft, c.MidRight}
+
+        Printer.Print("Total Bayar #" & FormatNumber(SubTotal, 0), arrWidth, arrFormat)
+        Printer.Print("Diskon #" & FormatNumber(0, 0), arrWidth, arrFormat)
+        Printer.Print("Diterima #" & FormatNumber(SubTotal, 0), arrWidth, arrFormat)
+        Printer.Print("Infaq #" & FormatNumber(0, 0), arrWidth, arrFormat)
+        Printer.Print("Kembalian #" & FormatNumber(0, 0), arrWidth, arrFormat)
+        Printer.Print("-----------------------------------")
+
+        Footer1 = {250, 0} : Footer2 = {c.MidCenter}
+        Printer.SetFont("Courier New", 8, FontStyle.Regular)
+        Printer.Print("Terima Kasih", Header1, Header2)
+        Printer.Print("Atas Kunjungan Anda", Header1, Header2)
+
+        Printer.DoPrint()
+
+    End Sub
+
+End Class
+
